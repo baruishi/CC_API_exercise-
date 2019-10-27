41760abbd5429f573f66aeeb0f8d881ad3d21232
baruishi <valduss@outlook.com>
2019-10-28 02:44:53 +0900

    API working, Readme written

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
 # CC_API_exercise-
+
+This was created during my time as a student at [Code Chrysalis](https://www.codechrysalis.io/).
+
+### UK Cities API
+
+<br>
+
+This API was craeted using on Postgres, GraphQL and Knex, written in JavaScript.
+
+It represents a simple API with cities in the United Kingdom.
+County, Region and Population is stored for every city.
+
+<br>
+
+### Set-up
+
+```
+
+yarn install
+
+yarn migrate
+
+yarn start
+
+```
+
+### How to use this API:
+
+# Show all cities in the databe.
+
+```
+{
+  cities {
+    city
+    county
+    region
+    population
+  }
+}
+```
+
+# Show a chosen city.
+
+```
+{
+  city( city: "London") {
+    city
+    county
+    region
+    population
+  }
+}
+```
+
+# Update a population of a chosen city.
+
+```
+mutation {
+  updatePopulationByCity( city: "London",  population: 3500000)
+}
+```
+
+# Add a new city.
+
+```
+mutation {
+  addCity( input: {
+  city: "NEW CITY"
+  county: "COUNTY"
+  region: "REGION"
+  population: 100000
+})
+}
+```
+
+# Delete a chosen city.
+
+```
+mutation {
+  delete(city: "CITY")
+}
+```
+
+developed by: _baruishi_
--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const config = require("./config");
 
 //database
 const knex = require("knex")(config.db);
-//const models = require("./models")(knex);
 
-//const apiRouter = require("./controllers")(models);
-//const morgan = require("morgan");
-//const bodyParser = require("body-parser");
 const express = require("express");
-//const root = require("./controllers");
 
 //graph iQL
 const app = express();
@@ -52,6 +47,7 @@ const schema = buildSchema(`
   type Mutation {
   addCity(input: addCity): String!
   updatePopulationByCity(city: String!, population: Int!): String!
+  delete(city: String!): String!
   }
 `);
 
@@ -76,6 +72,17 @@ const root = {
         return city;
       });
   },
+
+  delete: input => {
+    console.log(input.city);
+    return knex("cities")
+      .where({ city: input.city })
+      .del()
+      .then(() => {
+        return `City ${input.city} was delated from database`;
+      });
+  },
+
   addCity: input => {
     const newObj = input.input;
     return knex("cities")
@@ -89,12 +96,7 @@ const root = {
         return `New city: ${newObj.city} added to database`;
       });
   },
-  /*citiesByPopulation: (input) => { //TODO figure out how to select a city with popualtion grater than specific number
-    const populationOfCities = input.ppulation
-  },*/
   updatePopulationByCity: input => {
-    console.log("input", input);
-    console.log("input.population", input.population);
     const selectedCity = input.city;
     const newPopulation = input.population;
     return knex(`cities`)
@@ -118,42 +120,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
 });
-
-// //server
-// app.use("dev");
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Orign", "*");
-//   res.header(
-//     "Access-Controll-Allow-Deaders",
-//     "GET, PUT, POST, DELETE, OPTIONS, PATH"
-//   );
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, authorization"
-//   );
-//   next();
-// });
-
-// // 3. Parse request bodies as json
-// app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
-
-// 4. If the requests begin with '/api', hand them off to the API router
-// app.use("/api", apiRouter);
-// app.use(express.static(`${__dirname}/public`)); // otherwise load the client app
-
-// 5. Catch unhandled errors thrown by any of the previous middleware steps
-// eslint-disable-next-line no-unused-vars
-// app.use((err, req, res, next) => {
-//   if (err.stack) {
-//     if (err.stack.match("node_modules/body-parser"))
-//       return res.status(400).send("Invalid JSON");
-//   }
-
-//   services.logger.log(err);
-//   return res.status(500).send("Internal Error.");
-// });
-
-//start server
-// app.listen(config.express.port, () => {
-//   services.logger.log(`Server up and listening on port ${config.express.port}`);
-// });
